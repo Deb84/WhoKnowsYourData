4436d6f52188bec1bc3da236430e020689929bcb
deb84 <56758812+Deb84@users.noreply.github.com>
2026-09-03 00:08:30 +0200

    refactor: added repository interface to service

--- backend/services/values/service.go
+++ backend/services/values/service.go
@@ -5,17 +5,27 @@ import (
 	"context"
 	"fmt"
 	"whoknowsyourdata/domain"
-	neo4jrepo "whoknowsyourdata/repository/values/neo4j"
 
 	"github.com/google/uuid"
 )
 
-type ValueService struct {
-	log        domain.Logger
-	repository *neo4jrepo.ValueRepository
+type ValueRepository interface {
+	InitDB(ctx context.Context) error
+	CreateValue(ctx context.Context, value *domain.Value) error
+	DeleteValue(ctx context.Context, valueUUID uuid.UUID) error
+	CreateValues(ctx context.Context, values []domain.Value) error
+	GetValue(ctx context.Context, _uuid uuid.UUID) (*domain.Value, error)
+	GetValuesFromLabel(ctx context.Context, label string) ([]domain.Value, error)
+	CreateRelation(ctx context.Context, relation *domain.Relation, expectedRelations int) error
+	CreateRelations(ctx context.Context, relations []domain.Relation, expectedRelations int) error
 }
 
-func NewValueService(log domain.Logger, repository *neo4jrepo.ValueRepository) *ValueService {
+type ValueService struct {
+	log        domain.Logger
+	repository ValueRepository
+}
+
+func NewValueService(log domain.Logger, repository ValueRepository) *ValueService {
 
 	return &ValueService{
 		log:        log,
